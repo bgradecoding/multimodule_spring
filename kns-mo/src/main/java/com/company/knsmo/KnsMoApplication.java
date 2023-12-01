f5652e41671d5e2234f113db3177f450061223ea
bgradecoding <iinee12@kakao.com>
2023-12-01 23:24:40 +0900

    init

---
+++ kns-mo/src/main/java/com/company/knsmo/KnsMoApplication.java
@@ -0,0 +1,13 @@
+package com.company.knsmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnsMoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnsMoApplication.class, args);
+    }
+
+}
---
+++ kns-mo/src/test/java/com/company/knsmo/KnsMoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.knsmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnsMoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
